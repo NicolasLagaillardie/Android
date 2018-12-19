e97c9926564f5dd5328bef8dbacbb9b1f4cda836
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-12-19 11:33:28 +0100

    menu slide

--- LightBulb/app/src/main/java/com/example/lightbulb/ContextManagementActivity.java
+++ LightBulb/app/src/main/java/com/example/lightbulb/ContextManagementActivity.java
@@ -1,15 +1,28 @@
 package com.example.lightbulb;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContextManagementActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout;
 
     LightContextHttpManager lightContextHttpManager = new LightContextHttpManager(this);
 
@@ -18,29 +31,52 @@ public class ContextManagementActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        //Récupération du Spinner déclaré dans le fichier main.xml de res/layout
+        final Spinner spinner = (Spinner) findViewById(R.id.lights_spinner);
+
+        lightContextHttpManager.retrieveAllLights();
+
         ((Button) findViewById(R.id.buttonCheck)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String light = ((EditText) findViewById(R.id.editText1))
-                        .getText().toString();
+/*                String light = ((EditText) findViewById(R.id.editText1))
+                        .getText().toString();*/
+                String light = spinner.getSelectedItem().toString();
                 lightContextHttpManager.retrieveLightContextState(light);
             }
         });
+
         ((Button) findViewById(R.id.buttonSwitchLight)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String light = ((EditText) findViewById(R.id.editText1))
-                        .getText().toString();
+                String light = spinner.getSelectedItem().toString();
                 lightContextHttpManager.switchLight(light);
             }
         });
         ((Button) findViewById(R.id.buttonDelete)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String light = ((EditText) findViewById(R.id.editText1))
-                        .getText().toString();
+                String light = spinner.getSelectedItem().toString();
                 lightContextHttpManager.deleteLight(light);
             }
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,9 +98,25 @@ public class ContextManagementActivity extends Activity {
 
     }
 
+    public void onUpdateList(List listLights){
+
+        Spinner spinner = (Spinner) findViewById(R.id.lights_spinner);
+
+        //Création d'une liste d'élément à mettre dans le Spinner(pour l'exemple)
+        ArrayAdapter adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                listLights
+        );
+
+        /* On definit une présentation du spinner quand il est déroulé         (android.R.layout.simple_spinner_dropdown_item) */
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Enfin on passe l'adapter au Spinner et c'est tout
+        spinner.setAdapter(adapter);
+
+    }
+
     public void onDelete(){
-
-
 
         ((TextView) findViewById(R.id.textViewLightValue)).setText("");
         ((TextView) findViewById(R.id.textViewRoomIdValue)).setText("");
--- LightBulb/app/src/main/java/com/example/lightbulb/LightContextHttpManager.java
+++ LightBulb/app/src/main/java/com/example/lightbulb/LightContextHttpManager.java
@@ -1,17 +1,25 @@
 package com.example.lightbulb;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class LightContextHttpManager {
 
@@ -19,6 +27,54 @@ public class LightContextHttpManager {
 
     public LightContextHttpManager(ContextManagementActivity contextManagementActivity) {
         this.contextManagementActivity = contextManagementActivity;
+    }
+
+    private void warningMessage(VolleyError error){
+
+        CharSequence text = error.toString();
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contextManagementActivity, text, duration * 10);
+        toast.show();
+    }
+
+    public void retrieveAllLights(){
+
+        String url =  "https://faircorp-paul-breugnot.cleverapps.io/api/lights/";
+
+        RequestQueue queue = Volley.newRequestQueue(contextManagementActivity);
+
+        JsonArrayRequest contextRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+
+                            List listLights = new ArrayList();
+
+                            for (int i = 0; i < response.length(); ++i) {
+                                JSONObject rec = response.getJSONObject(i);
+                                int id = Integer.parseInt(rec.get("id").toString());
+                                listLights.add(id);
+                            }
+
+                            contextManagementActivity.onUpdateList(listLights);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        warningMessage(error);
+                    }
+                });
+        queue.add(contextRequest);
+
     }
 
     public void retrieveLightContextState(final String light){
@@ -49,7 +105,7 @@ public class LightContextHttpManager {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // Some error to access URL : Room may not exists...
+                        warningMessage(error);
                     }
                 });
         queue.add(contextRequest);
@@ -84,7 +140,7 @@ public class LightContextHttpManager {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // Some error to access URL : Room may not exists...
+                        warningMessage(error);
                     }
                 });
         queue.add(contextRequest);
@@ -110,8 +166,7 @@ public class LightContextHttpManager {
                 {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // error.
-
+                        warningMessage(error);
                     }
                 }
         );
@@ -147,7 +202,7 @@ public class LightContextHttpManager {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // Some error to access URL : Room may not exists...
+                        warningMessage(error);
                     }
                 });
         queue.add(contextRequest);
